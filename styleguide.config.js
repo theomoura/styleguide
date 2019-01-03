1c418d16d268be6b091eccd9b4749e99543c6026
Lucas Bebber <lucasbbebber@gmail.com>
2019-01-03 16:15:55 -0200

    Replace old Menu with internal ActionMenu Menu

--- styleguide.config.js
+++ styleguide.config.js
@@ -90,7 +90,6 @@ module.exports = {
           name: 'Forms',
           components: [
             'react/components/Button/index.js',
-            'react/components/Menu/index.js',
             'react/components/ButtonWithIcon/index.js',
             'react/components/Checkbox/index.js',
             'react/components/DatePicker/index.js',
